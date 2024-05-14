6254d7ad719ec5ccff8cba9c5aa36b87936d0a75
BDanielo <daniel@armieux.fr>
2024-05-14 12:17:16 +0200

    feat(github) : update jest workflow

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    reporters: [
+        'default',
+        ['jest-junit', {
+            outputDirectory: 'test-results/junit',
+            outputName: 'js-test-results.xml',
+        }],
+    ],
 };
